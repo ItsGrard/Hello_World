0196a9c32aa3c7374bf5237d075dde97c46f457e
ItsGrard <33222999+ItsGrard@users.noreply.github.com>
2017-11-23 19:23:20 +0100

    Create master.cpp

---
+++ master.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+		int height;
+		
+		cout << "Insert height: " << endl;
+			cin >> height;
+		
+		for (int fila = 1; fila <= height; fila++){
+					for (int i = 1; i <= height-fila; i++){
+							cout << " ";
+					}
+					for (int k = 1; k <= fila; k++){
+							cout << " *";
+					}
+		
+
+			cout << endl;
+		}
+return 0;
+}
